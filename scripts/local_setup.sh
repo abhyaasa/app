9c53db81ee202d4a83ffa250c6c998a2897b6c54
Chris Haynes <vasudeva.chaynes@gmail.com>
2016-02-15 14:00:28 -0500

    gitignore improvements

--- scripts/local_setup.sh
+++ scripts/local_setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Setup local iOS app development context. Called by full_setup.sh.
 ionic state restore # install dependencies specified in package.json
+bower install
 gulp flavor --name test # make sure we're using the test flavor
 # REVIEW Execute the following manually if it does not work from this script.
 gulp cmd -a ei # builds ios app and invokes emulator
